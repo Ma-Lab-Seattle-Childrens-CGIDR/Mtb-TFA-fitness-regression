b361f49836472dd421f0b29d087ecdea7c305e3b
Ethan Bustad <ethan.bustad@gmail.com>
2024-09-17 15:22:59 -0700

    Add iModulon inference method: attempt 1

---
+++ 1_network-inference/docker/imodulon/imodulon_execute.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ ! -f "${@:$#}" ]
+then
+	./modulome-nextflow/4_optICA/run_ica.sh -h
+	echo "FILE must come last."
+	exit 1
+fi
+
+indir="$(dirname "${@:$#}")"
+
+datamash transpose < "${@:$#}" > "${indir}/expression_file.tsv"
+
+cd modulome-nextflow/4_optICA
+
+./run_ica.sh "${@:1:$#}" "${indir}/expression_file.tsv"
+
+cd -
+
+rm "${indir}/expression_file.tsv"
